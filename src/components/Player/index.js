bebbf2fb97880c6088523fe6bfb571d9be5fcfa7
Paul Hine <paul.hine@gmail.com>
2018-01-06 12:39:29 -0500

    add play/pause to video

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -3,6 +3,14 @@ import cx from 'classnames'
 import styles from './styles.scss'
 import CloseIcon from '../../icons/close.svg'
 
+const videoDef = {
+  elem: null,
+}
+const bindVideo = e => {
+  videoDef.elem = e
+}
+const playPause = () => (videoDef.elem.paused ? videoDef.elem.play() : videoDef.elem.pause())
+
 const Player = ({ isActive, title, src, onClose }) =>
   <div className={cx(styles.player, { [styles.isActive]: isActive })}>
     <button className={styles.closeButton} onClick={onClose}>
@@ -11,7 +19,7 @@ const Player = ({ isActive, title, src, onClose }) =>
     <div className={styles.title}>
       {title}
     </div>
-    <video autoPlay muted loop src={src} playsInline />
+    <video autoPlay muted loop src={src} playsInline ref={e => bindVideo(e)} onClick={playPause} />
   </div>
 
 export default Player
