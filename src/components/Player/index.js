ade0cb866fec86c4ebf21346a70190d4119079ee
Paul Hine <paul.hine@gmail.com>
2018-01-06 13:19:27 -0500

    add video loading progress

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -1,25 +1,69 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import cx from 'classnames'
 import styles from './styles.scss'
 import CloseIcon from '../../icons/close.svg'
 
-const videoDef = {
-  elem: null,
+class Player extends PureComponent {
+  constructor(props) {
+    super(props)
+    this.videoElem = null
+    this.state = {
+      loadProgress: 0,
+      canPlayThrough: false,
+    }
+  }
+  bindVideo(e) {
+    this.videoElem = e
+  }
+  playPause() {
+    if (this.videoElem.paused) {
+      this.videoElem.play()
+    } else {
+      this.videoElem.pause()
+    }
+  }
+  updateLoadProgress() {
+    if (this.videoElem.readyState === 0) {
+      return
+    }
+    const endBuf = this.videoElem.buffered.end(0)
+    this.setState({
+      loadProgress: parseInt(endBuf / this.videoElem.duration * 100, 10),
+      canPlayThrough: this.videoElem.readyState === 4,
+    })
+  }
+  render() {
+    const { isActive, title, src, onClose } = this.props
+    return (
+      <div className={cx(styles.player, { [styles.isActive]: isActive })}>
+        <button className={styles.closeButton} onClick={onClose}>
+          <CloseIcon />
+        </button>
+        <div className={styles.title}>
+          {title}
+        </div>
+        <div
+          className={cx(styles.videoContainer, { [styles.isLoaded]: this.state.canPlayThrough })}
+        >
+          <p>
+            {this.state.loadProgress}%
+          </p>
+          <video
+            src={src}
+            autoPlay
+            muted
+            loop
+            playsInline
+            ref={e => this.bindVideo(e)}
+            onClick={() => this.playPause()}
+            onProgress={() => this.updateLoadProgress()}
+            onLoadStart={() => this.updateLoadProgress()}
+            onCanPlay={() => this.updateLoadProgress()}
+          />
+        </div>
+      </div>
+    )
+  }
 }
-const bindVideo = e => {
-  videoDef.elem = e
-}
-const playPause = () => (videoDef.elem.paused ? videoDef.elem.play() : videoDef.elem.pause())
-
-const Player = ({ isActive, title, src, onClose }) =>
-  <div className={cx(styles.player, { [styles.isActive]: isActive })}>
-    <button className={styles.closeButton} onClick={onClose}>
-      <CloseIcon />
-    </button>
-    <div className={styles.title}>
-      {title}
-    </div>
-    <video autoPlay muted loop src={src} playsInline ref={e => bindVideo(e)} onClick={playPause} />
-  </div>
 
 export default Player
