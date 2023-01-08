38244805a41820ad04aabfb1e2ca99f489342037
Paul Hine <paul@hine.works>
2023-01-08 15:08:42 +0100

    updates to works and site copy

--- src/components/Project/VideoThumbs.js
+++ src/components/Project/VideoThumbs.js
@@ -22,27 +22,34 @@ export default ({ work, target }) => {
       {videos.map(vid => {
         const ext =
           target.match('mobile') || vid.match('mobile') ? 'png' : 'gif'
-        return (
-          <Link
-            to={`/on/${work.slug}/play/${vid}.mp4`}
-            className={cx(
-              'videoThumb',
-              vid.match('mobile') ? 'mobile' : 'desktop',
-              {
-                smaller: videos.length > 2,
-              }
-            )}
-            key={target + vid}
-          >
-            <div className="playButton">
-              <PlayIcon />
+        const youtube = vid.match(/youtube\.com\/watch\?v=(.+)/)
+        return youtube
+          ? (
+            <div className='ytvideoWrapper'>
+              <iframe width="560" height="349" src={`https://www.youtube.com/embed/${youtube[1]}`} title="YouTube video player" frameborder="0" allow="fullscreen; web-share" allowfullscreen></iframe>
             </div>
-            <img
-              src={`${config.mediaUrl}/${work.slug}/${vid}-thumb.${ext}`}
-              alt={`${target} video thumb`}
-            />
-          </Link>
-        )
+          )
+          : (
+            <Link
+              to={`/on/${work.slug}/play/${vid}.mp4`}
+              className={cx(
+                'videoThumb',
+                vid.match('mobile') ? 'mobile' : 'desktop',
+                {
+                  smaller: videos.length > 2,
+                }
+              )}
+              key={target + vid}
+            >
+              <div className="playButton">
+                <PlayIcon />
+              </div>
+              <img
+                src={`${config.mediaUrl}/${work.slug}/${vid}-thumb.${ext}`}
+                alt={`${target} video thumb`}
+              />
+            </Link>
+          )
       })}
     </MediaQuery>
   )
