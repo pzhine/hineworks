7d5f4eb2e4be974b0c6d7019e58237b3a3d362ab
Paul Hine <paul.hine@gmail.com>
2024-09-04 16:13:47 +0200

    update media display and fonts

--- src/components/Project/Thumb.js
+++ src/components/Project/Thumb.js
@@ -1,29 +1,41 @@
-import React from 'react'
-import cx from 'classnames'
-import MediaQuery from 'react-responsive'
-import config from '../../content/config.json'
-import styles from './styles.scss'
+import React from "react";
+import cx from "classnames";
+import MediaQuery from "react-responsive";
+import config from "../../content/config.json";
+import styles from "./styles.scss";
+import { Link } from "react-router-dom";
 
-export default ({ work, target }) => {
-  const { mobile } = config.breakpoints
-  const mq = target.match('mobile')
+export default ({ work, target, linked }) => {
+  const { mobile } = config.breakpoints;
+  const mq = target.match("mobile")
     ? { maxWidth: mobile - 1 }
-    : { minWidth: mobile }
+    : { minWidth: mobile };
   return (
     <MediaQuery {...mq}>
-      {work.media[target].index.map(src => {
-        const ext = src.match('photo') ? 'jpg' : 'png'
+      {work.media[target].index.map((src) => {
+        const ext = src.match("photo") ? "jpg" : "png";
+        if (linked) {
+          return (
+            <Link to={`/on/${work.slug}/play/${src}.${ext}`} key={target + src}>
+              <img
+                className={cx("main-photo", target)}
+                src={`${config.mediaUrl}/${work.slug}/${src}.${ext}`}
+                alt={"detail photo thumbnail"}
+              />
+            </Link>
+          );
+        }
         return (
           <div
             style={{
               backgroundImage: `url('${config.mediaUrl}/${work.slug}/${src}.${ext}')`,
             }}
-            className={cx('thumb', target, {
+            className={cx("thumb", target, {
               twoRows: work.media[target].index.length === 2,
             })}
           />
-        )
+        );
       })}
     </MediaQuery>
-  )
-}
+  );
+};
--- src/components/Project/VideoThumbs.js
+++ src/components/Project/VideoThumbs.js
@@ -1,56 +1,66 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import cx from 'classnames'
-import MediaQuery from 'react-responsive'
-import config from '../../content/config.json'
-import styles from './styles.scss'
+import React from "react";
+import { Link } from "react-router-dom";
+import cx from "classnames";
+import MediaQuery from "react-responsive";
+import config from "../../content/config.json";
+import styles from "./styles.scss";
 
-import PlayIcon from '../../icons/play.svg'
+import PlayIcon from "../../icons/play.svg";
+import Thumb from "./Thumb";
 
 export default ({ work, target }) => {
-  const { mobile } = config.breakpoints
-  const mq = target.match('mobile')
+  const { mobile } = config.breakpoints;
+  const mq = target.match("mobile")
     ? { maxWidth: mobile - 1 }
-    : { minWidth: mobile }
-  const { videos } = work.media[target]
+    : { minWidth: mobile };
+  const { videos } = work.media[target];
   return (
     <MediaQuery
       {...mq}
-      className={cx('videoThumbs', styles[target])}
+      className={cx("videoThumbs", styles[target])}
       component="div"
     >
-      {videos.map(vid => {
+      {videos.map((vid) => {
+        if (vid.match("photo")) {
+          return <Thumb work={work} target={target} key={"dtm"} linked />;
+        }
         const ext =
-          target.match('mobile') || vid.match('mobile') ? 'png' : 'gif'
-        const youtube = vid.match(/youtube\.com\/watch\?v=(.+)/)
-        return youtube
-          ? (
-            <div className='ytvideoWrapper'>
-              <iframe width="560" height="349" src={`https://www.youtube.com/embed/${youtube[1]}`} title="YouTube video player" frameborder="0" allow="fullscreen; web-share" allowfullscreen></iframe>
+          target.match("mobile") || vid.match("mobile") ? "png" : "gif";
+        const youtube = vid.match(/youtube\.com\/watch\?v=(.+)/);
+        return youtube ? (
+          <div className="ytvideoWrapper">
+            <iframe
+              width="560"
+              height="349"
+              src={`https://www.youtube.com/embed/${youtube[1]}`}
+              title="YouTube video player"
+              frameborder="0"
+              allow="fullscreen; web-share"
+              allowfullscreen
+            ></iframe>
+          </div>
+        ) : (
+          <Link
+            to={`/on/${work.slug}/play/${vid}.mp4`}
+            className={cx(
+              "videoThumb",
+              vid.match("mobile") ? "mobile" : "desktop",
+              {
+                smaller: videos.length > 2,
+              }
+            )}
+            key={target + vid}
+          >
+            <div className="playButton">
+              <PlayIcon />
             </div>
-          )
-          : (
-            <Link
-              to={`/on/${work.slug}/play/${vid}.mp4`}
-              className={cx(
-                'videoThumb',
-                vid.match('mobile') ? 'mobile' : 'desktop',
-                {
-                  smaller: videos.length > 2,
-                }
-              )}
-              key={target + vid}
-            >
-              <div className="playButton">
-                <PlayIcon />
-              </div>
-              <img
-                src={`${config.mediaUrl}/${work.slug}/${vid}-thumb.${ext}`}
-                alt={`${target} video thumb`}
-              />
-            </Link>
-          )
+            <img
+              src={`${config.mediaUrl}/${work.slug}/${vid}-thumb.${ext}`}
+              alt={`${target} video thumb`}
+            />
+          </Link>
+        );
       })}
     </MediaQuery>
-  )
-}
+  );
+};
